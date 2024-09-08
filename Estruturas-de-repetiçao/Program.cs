910c7f82fd2c1d5b153d4bc85bd4274b5ee6cccb
Lucas Santana <163876124+LucasAlexSant@users.noreply.github.com>
2024-09-08 16:09:38 -0300

    mais liçoes feitas

---
+++ Estruturas-de-repetiçao/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// int num = 5;
+// for (int contador =0; contador <= 10; contador++){
+//     Console.WriteLine($"{num} x {contador} = {num * contador}");
+// }
+
+
+
+int num = 5;
+int contador = 0;
+
+while(contador <= 10){
+    Console.WriteLine($" {num} x {contador} = {num * contador}");
+    contador++;
+}
