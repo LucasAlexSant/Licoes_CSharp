f4f79b51bebd8b91ac4a67bfa04199aad4647ffa
Lucas Santana <163876124+LucasAlexSant@users.noreply.github.com>
2024-09-08 10:26:33 -0300

    sintaxe e tipos de dados em C#

---
+++ CriandoClasses/Program.cs
@@ -0,0 +1,19 @@
+﻿using CriandoClasses.Models;
+
+
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+
+dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual);
+
+// string apresentacao = "Olá, seja bem vindo";
+// int quantidade = 1;
+
+// Console.WriteLine(apresentacao + quantidade);
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Lucas";
+// pessoa1.Idade = 20;
+// pessoa1.apresentar();
--- CriandoClasses/obj/Debug/net8.0/CriandoClasses.AssemblyInfo.cs
+++ CriandoClasses/obj/Debug/net8.0/CriandoClasses.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Primeiro-codigo")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CriandoClasses")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+896263fac036bfe96bc878e52e92c6c0616ac1e6")]
-[assembly: System.Reflection.AssemblyProductAttribute("Primeiro-codigo")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Primeiro-codigo")]
+[assembly: System.Reflection.AssemblyProductAttribute("CriandoClasses")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CriandoClasses")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
